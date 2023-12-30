574ff4bf5edfebef13190282b9eb90e1cce90530
romys <romysaputrasihanandaa@gmail.com>
2023-12-30 17:26:02 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 from json import dumps
+from dotenv import load_dotenv
+import os
+load_dotenv() 
+cookie = os.getenv("COOKIE") 
 
 params = {
     "can_support_threading": True,
@@ -11,8 +15,7 @@ params = {
 }
 
 headers = {
-    # 'Cookie': 'ig_nrcb=1; mid=ZSZi_gAEAAHXcCtsxv3cG8biTwgH; csrftoken=ZA4OXT11XEKVGS2sp9GAjElCwLpMAn5p; ds_user_id=46719067093; dpr=1.100000023841858',
-    'Cookie': 'ig_did=C37733B2-2853-40A7-B3B1-70BBA58EBEA7; ig_nrcb=1; mid=ZSZi_gAEAAHXcCtsxv3cG8biTwgH; datr=_GImZcBIvUSXBBx6325xMiJH; ds_user_id=46719067093; dpr=1.100000023841858; shbid="15857\05446719067093\0541735462074:01f75729e3d962279bf746c9b9146fc7c61da319b01bc7eff14bce2087bf8e3c84411647"; shbts="1703926074\05446719067093\0541735462074:01f748b6143e0bfdf1e3d48a19d93bc85baa93c8504e5fef41a004dcbb0043de5b19709e"; csrftoken=eU7hjKHq1zXyNN8NdJoC7qIDK5Fq7emX; sessionid=46719067093%3AvXNDOezyRKUqjo%3A29%3AAYcGNBHS33I8sxt5ODQms_HHpxuESd2fThVOD_0pEA; rur="NHA\05446719067093\0541735465749:01f782cfcd3a549434f95ef7d3f746d60d1ee47493a68b4180002d1e675d31b1a4c63e97"',
+    'Cookie': cookie,
     'User-Agent':'Instagram 126.0.0.25.121 Android (23/6.0.1; 320dpi; 720x1280; samsung; SM-A310F; a3xelte; samsungexynos7580; en_GB; 110937453)',
 }
 
