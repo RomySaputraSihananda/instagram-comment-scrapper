c80ff7cdb88a6f1a9580fd35d894c07301698a8c
romys <romysaputrasihanandaa@gmail.com>
2023-12-31 17:42:25 +0700

    update

--- comment/Comment.py
+++ comment/Comment.py
@@ -5,10 +5,14 @@ from json import dumps
 from dotenv import load_dotenv
 from time import sleep
 
+from comment.helpers import logging
+
 class Comment:
     def __init__(self, cookie: str) -> None:
         self.__min_id: str =  None
         self.__requests : Session = Session()
+        self.__result: dict = {}
+        self.__result['comments']: list = []
         
         self.__requests.headers.update({
             "Cookie": cookie,
@@ -33,23 +37,42 @@ class Comment:
     
     def __get_reply_comment(self, comment_id: str):
         min_id: str = ''
+        child_comments: list = []
 
         while True:
             response: Response = self.__requests.get(f'https://www.instagram.com/api/v1/media/{self.__media_id}/comments/{comment_id}/child_comments/?min_id={min_id}').json()
-
+            
             for comment in response['child_comments']:
-                print(f'\t=>{comment["text"]}')
-                sleep(1)
+                child_comments.append({
+                    "username": comment["user"]["username"],
+                    "full_name": comment["user"]["full_name"],
+                    "comment": comment["text"],
+                    "create_time": comment["created_at"],
+                    "avatar": comment["user"]["profile_pic_url"],
+                    "total_like": comment["comment_like_count"],
+                })
 
             if(not response['has_more_head_child_comments']): break
             
             min_id: str = response['next_min_child_cursor'] 
 
+            sleep(1)
+        return child_comments
+
     def __filter_comments(self, response: dict) -> None:
         for comment in response['comments']:
-            print(comment['text'])
+            logging.info(comment['text'])
 
-            if(comment['child_comment_count']): self.__get_reply_comment(comment['pk'])
+            self.__result['comments'].append({
+                "username": comment["user"]["username"],
+                "full_name": comment["user"]["full_name"],
+                "comment": comment["text"],
+                "create_time": comment["created_at"],
+                "avatar": comment["user"]["profile_pic_url"],
+                "total_like": comment["comment_like_count"],
+                "total_reply": comment["child_comment_count"],
+                "replies": self.__get_reply_comment(comment['pk']) if comment['child_comment_count'] else [] 
+            })
             
             sleep(1)
 
@@ -62,8 +85,10 @@ class Comment:
         self.__media_id = self.__dencode_media_id(post_id)
         while(True):
             response: Response = self.__requests.get(f'https://www.instagram.com/api/v1/media/{self.__media_id}/comments/', params=self.__build_params())
-            
+
             if(self.__filter_comments(response.json())): break
+        
+        return self.__result
 
 
 # testing
@@ -73,4 +98,21 @@ if(__name__ == '__main__'):
 
     comment: Comment = Comment(cookie)
     # comment.excecute('C1ACfnvh4KE')
-    comment.excecute('C1Ww1LChZhN')
+    # comment.excecute('C1Ww1LChZhN')
+    data: dict = comment.excecute('C1f8TcBhGSi')
+    with open('test_data.json', 'w') as file:
+        file.write(dumps(data, indent=2, ensure_ascii=False))
+
+# "caption": "mau oleng ke siapapun tetep aja pulang lagi ke freya😣 #freyajkt48 #freyajayawardana #fypシ ",
+# "date_now": "2023-12-22T21:24:08",
+# "video_url": "https://t.tiktok.com/i18n/share/video/7299013864170032390/?_d=0&comment_author_id=6789225331385418754&mid=6703154980951427074&preview_pb=0&region=ID&share_comment_id=7299627887044461317&share_item_id=7299013864170032390&sharer_language=en&source=h5_t&u_code=0",
+# "comments": [
+# {
+#     "username": "ryzzhighinthesky",
+#     "nickname": "Not Ryz",
+#     "comment": "freya aku mau cbr😣😣",
+#     "create_time": "2023-11-10T07:44:48",
+#     "avatar": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4c3c29930e3420f1119852fee1767dea~c5_100x100.jpg?x-expires=1703340000&x-signature=j00DUxk%2B8pIXiUjAO3n%2Bs9dOWjo%3D",
+#     "total_reply": 0,
+#     "replies": []
+# },')
